96ad8abe59a5eb91d8c487f5c82ccd959cdc755e
root <root@host-7-17.linuxzoo.net>
2016-05-10 08:21:51 +0100

    Exercise 12: Some more methods of raw_input() & %r

---
+++ ex12.py
@@ -0,0 +1,15 @@
+#Exercise 12: Prompting People
+
+age =  raw_input('How old are you? ')
+height = raw_input('How tall are you? ')
+weight = raw_input("How much do you weight? ")
+
+print 'So, you\'re %r old, %r tall and %r weight.' %(age,height,weight)
+
+
+# noticed differnce for %r
+#age = int( raw_input('How old are you? '))
+#height = int(raw_input('How tall are you? '))
+#weight = int(raw_input("How much do you weight? "))
+
+#print 'So, you\'re %d old, %d tall and %d weight.' %(age,height,weight)
