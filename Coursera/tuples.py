6fb9f7fea1b8ebe590426fcd78e66f52327499ac
root <root@host-7-49.linuxzoo.net>
2016-11-07 04:49:35 +0000

    Tuples

---
+++ Coursera/tuples.py
@@ -0,0 +1,18 @@
+#10.2 Write a program to read through the mbox-short.txt and figure out the distribution by hour of the day for each of the messages. You can pull the hour out from the 'From ' line by finding the time and then splitting the string a second time using a colon.
+# "From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008"
+#Once you have accumulated the counts for each hour, print out the counts, sorted by hour.
+
+name = raw_input("Enter file: ")
+fh = open(name,'r')
+dct = dict()
+lst = list()
+for ln in fh:
+	if ln.startswith('From '):
+		ln = ln.split()
+		ln = ln[5].split(':')
+		dct[ln[0]] = dct.get(ln[0],0) + 1
+
+#t = dct.items()
+for i,j  in sorted(dct.items()): # we can use sorted(t), as we assign dct.items() to variable 't'.
+	print i,j
+
