6214c4793a0d418eca6cb1430ca2a65433c48fef
root <root@host-7-41.linuxzoo.net>
2016-11-16 06:21:01 +0000

    Using re.search() like startswith()

---
+++ Coursera/practice3.py
@@ -0,0 +1,9 @@
+# Using re.search() like startswith()
+import re
+hand  = open('mbox-short.txt')
+
+for line in hand:
+	line = line.strip()
+	if line.startswith('From: '):
+#	if re.search('^From: ', line): # we can use re.search() like this insted of startswith()
+		print line
