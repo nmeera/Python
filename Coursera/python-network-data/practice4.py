cdad9426697587982a1e8059d59adc05ba94c0ef
root <root@host-7-25.linuxzoo.net>
2016-11-18 10:33:20 +0000

    # Usage of re.findall(), Wild-Card Characters

--- Coursera/python-network-data/practice4.py
+++ Coursera/python-network-data/practice4.py
@@ -1,5 +1,10 @@
+# Usage of re.findall(), Wild-Card Characters 
 # Use X-P4Text.txt as input file  :)
+import re
 file =  raw_input("Enter the File Name: ")
 fh = open(file)
 for ln in fh:
-	print ln.strip()
+	ln = ln.strip()
+#	print re.findall('^X.*:', ln)
+# prints line which are starts with X, dot '.' is Wild-Card Characters here, '*' means all
+	print re.findall('^X.\S+:', ln) # here \S means non-blank char(no space) 
