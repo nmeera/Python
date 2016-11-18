f4f53149f1b2b6541688d42f19937a912060feaa
root <root@host-7-25.linuxzoo.net>
2016-11-18 11:45:12 +0000

    Matching and Extracting data

--- Coursera/python-network-data/practice5.py
+++ Coursera/python-network-data/practice5.py
@@ -1,7 +1,13 @@
 # Matching and Extracting data
-
+#When we use re.findall(), it returns a list of zeros or more sub-strings that match the regex. 
 import re 
 
 x = 'My 2 favorite numbers are 19 and 42'
-y = re.findall('[0-9]+',x)
+y = re.findall('[0-9]+',x) # see the difference of '+' with &with out 
+print y
+
+y  = re.findall('[AEIOU]', x)
+print y
+
+y  = re.findall('[aeiou]+', x)
 print y
