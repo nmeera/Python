30b1732d43767c16f4e0717070906d9373d81cd6
root <root@server2.example.com>
2016-12-07 18:26:31 +0530

    Scraping Numbers from HTML using BeautifulSoup, count of contents

--- Coursera/python-network-data/assignment12_a.py
+++ Coursera/python-network-data/assignment12_a.py
@@ -8,15 +8,19 @@ html = urllib.urlopen(url).read()
 soup = BeautifulSoup(html)
 
 # Retrieve all of the anchor tags
-tags = soup('a')
+tags = soup('span')
 
 #for tag in tags:
-#        #Look at the parts of tag
+ #      #Look at the parts of tag
 #	print 'TAG: ',tag
 #	print 'URL: ',tag.get('href', None)
 #	print 'Contents:', tag.contents[0]
 #	print 'Attrs: ',tag.attrs
 #	print "\n\n"
-
-for i in tags:
-	print i
+sum1 = 0
+for tag in tags:
+#	print tag
+#	print 'URL: ',tag.get('href', None)
+	i = tag.contents[0]
+	sum1 = sum1 + int(i)
+print "total: ", sum1	
