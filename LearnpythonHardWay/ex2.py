f6ddf137a9346304c093de47bc1e36273d858337
root <root@server2.example.com>
2017-02-06 23:06:11 +0530

    added a new line

--- LearnpythonHardWay/ex2.py
+++ LearnpythonHardWay/ex2.py
@@ -2,3 +2,4 @@ print " I could have code like this " # the comment is ignored
 # you can also use a comment  to "disable" or comment out a piece of code 
 # print "this won't run"
 print "this will run"
+print "ADDING NEW LINE"
